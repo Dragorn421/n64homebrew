c386cf6bb17c208176d0ec9f94769d56302492d6
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-04-11 09:06:09 +0200

    cc_mul_overflow_validate_test more tests

--- cc_mul_overflow_validate_test/main.c
+++ cc_mul_overflow_validate_test/main.c
@@ -50,8 +50,10 @@ int main()
         (1, 0, COMBINED, 0), (0, 0, 0, 1)));
     rdpq_fill_rectangle(posX, posY, posX + 32, posY + 32);
 
+    posY += 40;
+    posX = 10;
+
     rdpq_debug_log_msg("1 PRIM ENV PRIM -> mul=COMBINED");
-    posX += 40;
     rdpq_mode_combiner(RDPQ_COMBINER2(
         (1, PRIM, ENV, PRIM), (0, 0, 0, 1),
         (1, 0, COMBINED, 0), (0, 0, 0, 1)));
@@ -68,6 +70,13 @@ int main()
     posX += 40;
     rdpq_mode_combiner(RDPQ_COMBINER2(
         (0, 0, ENV, 1), (0, 0, 0, 1),
+        (PRIM, 0, COMBINED, 0), (0, 0, 0, 1)));
+    rdpq_fill_rectangle(posX, posY, posX + 32, posY + 32);
+
+    rdpq_debug_log_msg("0 0 ENV PRIM -> mul=COMBINED");
+    posX += 40;
+    rdpq_mode_combiner(RDPQ_COMBINER2(
+        (0, 0, ENV, PRIM), (0, 0, 0, 1),
         (1, 0, COMBINED, 0), (0, 0, 0, 1)));
     rdpq_fill_rectangle(posX, posY, posX + 32, posY + 32);
 
