e8f07238bc7beed2fe5ebeaa19bbe8c0d32a12ad
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-06-09 19:31:43 +0200

    test_aa_cutout: comments

--- test_aa_cutout/main.c
+++ test_aa_cutout/main.c
@@ -11,7 +11,11 @@ int main() {
     rdpq_debug_start();
     rdpq_debug_log(true);
 
-    uint16_t tlut[2] = {0x0000, 0xFFFF};
+    uint16_t tlut[2] = {
+        0x0000, // transparent black
+        0xFFFF, // opaque white
+    };
+    // generate a texture with a transparent black diagonal and white elsewhere
     uint8_t tex_buffer[32][32];
     for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 32; j++) {
@@ -33,6 +37,8 @@ int main() {
         rdpq_tex_upload(TILE0, &tex, &(rdpq_texparms_t){});
         rdpq_tex_upload_tlut(tlut, 0, 2);
 
+        // No AA nor alpha compare
+
         rdpq_texture_rectangle(TILE0, 10, 10, 42, 42, 0, 0);
         float v1[] = {50.0f, 10.0f, 0.0f, 0.0f, 1.0f};
         float v2[] = {50.0f, 50.0f, 0.0f, 32.0f, 1.0f};
@@ -40,6 +46,8 @@ int main() {
         float v4[] = {90.0f, 50.0f, 32.0f, 32.0f, 1.0f};
         rdpq_triangle(&TRIFMT_TEX, v1, v2, v3);
         rdpq_triangle(&TRIFMT_TEX, v4, v2, v3);
+
+        // No AA, alpha compare
 
         rdpq_mode_alphacompare(120);
 
@@ -51,6 +59,8 @@ int main() {
         rdpq_triangle(&TRIFMT_TEX, v1b, v2b, v3b);
         rdpq_triangle(&TRIFMT_TEX, v4b, v2b, v3b);
 
+        // AA and alpha compare
+
         rdpq_mode_antialias(AA_STANDARD);
 
         rdpq_texture_rectangle(TILE0, 10, 110, 42, 142, 0, 0);
@@ -60,6 +70,9 @@ int main() {
         float v4c[] = {90.0f, 150.0f, 32.0f, 32.0f, 1.0f};
         rdpq_triangle(&TRIFMT_TEX, v1c, v2c, v3c);
         rdpq_triangle(&TRIFMT_TEX, v4c, v2c, v3c);
+
+        // Still AA and alpha compare but the rdpq_mode_tlut call breaks alpha
+        // compare somehow
 
         rdpq_mode_tlut(TLUT_RGBA16);
 
