a8642acfb783a4525ca031fa6cecc2eef8aec9c9
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-08-30 01:07:25 +0200

    dsofmv: use `--gc-keep-exported` instead of the constructor hack

--- dsofmv/dso.c
+++ dsofmv/dso.c
@@ -1,13 +1,6 @@
 #include <libdragon.h>
 
-__attribute__((constructor)) void wrap_fmv_play(void) {
-  static int is_constructor_call = 1;
-  if (is_constructor_call) {
-    debugf("called by constructor: ignoring\n");
-    is_constructor_call = 0;
-    return;
-  }
-
+__attribute__((visibility("default"))) void wrap_fmv_play(void) {
   video_register_codec(&h264_codec);
   fmv_play("rom:/video.h264", &(fmv_parms_t){.loop = true});
 }
